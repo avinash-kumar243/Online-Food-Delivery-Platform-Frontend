8b7a42a58b94bd11c4be72af0749c202afa89c21
Avinash kumar <avinashkumar40977@gmail.com>
2026-04-22 08:14:05 +0530

    [AVINASH] : Updated the app component details

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { CustomerAuthComponent } from './components/auth/customer-auth/customer-auth';
+import { RestaurantAuthComponent } from './components/auth/restaurant-auth/restaurant-auth';
+import { DeliveryPartnerAuthComponent } from './components/auth/delivery-partner-auth/delivery-partner-auth';
+import { Welcome} from './components/welcome/welcome';
+import { Dashboard } from './components/dashboard/dashboard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+     { path: 'welcome', component: Welcome },
+
+    { path:'auth/customer', component: CustomerAuthComponent },
+    { path: 'auth/restaurant', component: RestaurantAuthComponent },
+    { path: 'auth/delivery-partner', component: DeliveryPartnerAuthComponent },
+
+    { path: 'dashboard', component: Dashboard }
+];
