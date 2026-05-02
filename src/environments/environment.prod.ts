408dda26fd7adeb95166279d9829caa3208a4397
Avinash kumar <avinashkumar40977@gmail.com>
2026-05-02 14:23:42 +0530

    [AVINASH] : Updated production environment configuration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,7 +6,10 @@ export const environment = {
   restaurantBaseUrl: 'http://localhost:8080/api/v1',
   menuBaseUrl: 'http://localhost:8080/api/v1',
   cartBaseUrl: 'http://localhost:8080/api/v1',
+  cartServiceDirectBaseUrl: 'http://localhost:8084/api/v1',
   orderBaseUrl: 'http://localhost:8080/api/v1',
+  orderServiceDirectBaseUrl: 'http://localhost:8085/api/v1',
   paymentBaseUrl: 'http://localhost:8080',
+  paymentServiceDirectBaseUrl: 'http://localhost:8086',
   deliveryBaseUrl: 'http://localhost:8080/api/v1'
 };
