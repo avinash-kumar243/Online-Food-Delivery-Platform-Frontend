810089b84fa426d3f4e4f4685613431659c8f428
Avinash kumar <avinashkumar40977@gmail.com>
2026-05-04 15:03:19 +0530

    [AVINASH] : Updated Angular environment API gateway configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,17 +1,5 @@
 export const environment = {
   production: false,
   apiGatewayBaseUrl: 'http://localhost:8080',
-  backendBaseUrl: 'http://localhost:8080',
-  authBaseUrl: 'http://localhost:8080',
-  restaurantBaseUrl: 'http://localhost:8080/api/v1',
-  menuBaseUrl: 'http://localhost:8080/api/v1',
-  cartBaseUrl: 'http://localhost:8080/api/v1',
-  cartServiceDirectBaseUrl: 'http://localhost:8084/api/v1',
-  orderBaseUrl: 'http://localhost:8080/api/v1',
-  orderServiceDirectBaseUrl: 'http://localhost:8085/api/v1',
-  paymentBaseUrl: 'http://localhost:8080',
-  paymentServiceDirectBaseUrl: 'http://localhost:8086',
-  deliveryBaseUrl: 'http://localhost:8080/api/v1',
-  reviewBaseUrl: 'http://localhost:8080',
-  reviewServiceDirectBaseUrl: 'http://localhost:8089'
+  apiBaseUrl: 'http://localhost:8080/api/v1'
 };
